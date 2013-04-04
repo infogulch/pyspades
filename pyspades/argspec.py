29913dffba556b6d08f5494a90c50ca958ae260f
infogulch <infogulch@gmail.com>
2013-04-04 17:14:28 -0500

    Fix argspec.py to work with methods

--- pyspades/argspec.py
+++ pyspades/argspec.py
@@ -2,12 +2,15 @@ import inspect
 
 def set(func):
     if not hasattr(func, 'argspec'):
+        if inspect.ismethod(func):
+            func = func.__func__
         func.argspec = inspect.getargspec(func)
 
 def iscompat(func, lenargs):
     spec = func.argspec
-    minargs = len(spec.args) - len(spec.defaults or ())
-    maxargs = len(spec.args) if spec.varargs is None else float("infinity")
+    lenspec = len(spec.args) - int(inspect.ismethod(func))
+    minargs = lenspec - len(spec.defaults or ())
+    maxargs = lenspec if spec.varargs is None else float("infinity")
     return minargs <= lenargs <= maxargs
 
 class ArgCountError(Exception):
